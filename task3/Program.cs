72d54aee0df113a2b9dc1e574c6348145a22be55
Anna <ds_anna@bk.ru>
2022-05-25 14:57:35 +0300

    Показать количество нечетных\четных чисел в массиве

---
+++ task3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задать массив, заполнить случайными положительными трёхзначными числами. 
+// Показать количество нечетных\четных чисел
+int[] arr = new int[5];
+int index1=0; // для нечетных чисел
+int index2=0; //для четных чисел
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = new Random().Next(100, 1000);
+    Console.Write($"{arr[i]} ");
+    if (arr[i] % 2 == 0) index1++;
+    else index2++;
+}
+Console.WriteLine();
+Console.WriteLine($"Количество четных чисел = {index1}");
+Console.WriteLine($"Количество нечетных чисел = {index2}");
